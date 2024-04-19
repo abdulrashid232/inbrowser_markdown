5d076b789ff5654745e64d2fac3798403a65da76
Abdel <77060081+abdulrashid232@users.noreply.github.com>
2024-04-19 11:33:03 +0000

    added custom colors to tailwind and created my components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: { colors: {
+      // Grayscale colors
+      'dark-gray': '#151619',
+      'mid-gray-1': '#35393F',
+      'mid-gray-2': '#5A6069',
+      'light-gray-1': '#7C8187',
+      'light-gray-2': '#C1C4CB',
+      'white': '#ffffff',
+  
+      // Accent colors
+      'orange': '#E46643',
+      'red': '#F39765',
+    },},
   },
   plugins: [],
 }
