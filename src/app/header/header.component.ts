7624ae013d9d0455fbe4846af787382e49b3c0c2
Abdel <77060081+abdulrashid232@users.noreply.github.com>
2024-04-20 18:12:36 +0000

    finished designing the sidebar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,4 +9,36 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  ngOnInit() {}
+  openNav() {
+    const sidebar = document.getElementById("Sidebar");
+    const header = document.querySelector("header");
+    const menuButton = document.getElementById("menuButton");
+    const closeButton = document.getElementById("closeButton");
+    const save_Delete_Div = document.getElementById("save&deleteDocument");
+    
+    sidebar.style.width = "250px";
+    header.style.marginLeft = "250px";
+    menuButton.classList.add('hidden'); 
+    closeButton.classList.remove('hidden'); 
+    closeButton.classList.add('flex'); 
+    save_Delete_Div.classList.add('hidden'); 
+  }
+  
+  closeNav() {
+    const sidebar = document.getElementById("Sidebar");
+    const header = document.querySelector("header");
+    const menuButton = document.getElementById("menuButton");
+    const closeButton = document.getElementById("closeButton");
+    const save_Delete_Div = document.getElementById("save&deleteDocument");
+    sidebar.style.width = "0";
+    header.style.marginLeft = "0";
+    menuButton.classList.remove('hidden'); 
+    closeButton.classList.remove('flex'); 
+    closeButton.classList.add('hidden'); 
+    save_Delete_Div.classList.remove('hidden');
+  }
+  
 }
+
+
