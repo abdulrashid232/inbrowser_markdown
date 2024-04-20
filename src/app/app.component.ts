5f1cf9335f74e9d2d9a73f002dfe75bcbcb576e7
Abdel <77060081+abdulrashid232@users.noreply.github.com>
2024-04-20 16:13:54 +0000

    finish designing the header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { provideMarkdown, MarkdownModule } from 'ngx-markdown';
+import { MarkdownModule } from 'ngx-markdown';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule,CommonModule,MarkdownModule],
+  imports: [RouterOutlet,FormsModule,CommonModule,MarkdownModule,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
